dbc8a3e1fc87cd04a21674fb16a26cab765c2a74
spratay <spratay@linuxmail.com>
2022-12-05 17:13:59 +0300

    test

--- twrp_S98Pro.mk
+++ twrp_S98Pro.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/doogee/S98Pro/device.mk)
 
 PRODUCT_DEVICE := S98Pro
-PRODUCT_NAME := omni_S98Pro
+PRODUCT_NAME := twrp_S98Pro
 PRODUCT_BRAND := DOOGEE
 PRODUCT_MODEL := S98Pro
 PRODUCT_MANUFACTURER := doogee
