51c7c65f7f71066eb79881493e0120ecfeadf6f2
spratay <spratay@linuxmail.com>
2022-12-05 16:16:31 +0300

    fuck

--- twrp_S98Pro.mk
+++ twrp_S98Pro.mk
@@ -1,50 +1,29 @@
 #
-# Copyright (C) 2021 The TWRP Open Source Project
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
-# Inherit from our custom product configuration
+# Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Device specific configs
-#$(call inherit-product, device/doogee/S98Pro/device.mk)
+# Inherit from S98Pro device
+$(call inherit-product, device/doogee/S98Pro/device.mk)
 
-## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := S98Pro
-PRODUCT_NAME := twrp_S98Pro
+PRODUCT_NAME := omni_S98Pro
 PRODUCT_BRAND := DOOGEE
 PRODUCT_MODEL := S98Pro
 PRODUCT_MANUFACTURER := doogee
 
-# Dynamic
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_GMS_CLIENTID_BASE := android-doogee
 
-# fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="S98Pro-user 12 SP1A.210812.016 1665738746 release-keys"
 
-# HACK: Set vendor patch level
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31 \
-    ro.bootimage.build.date.utc=0 \
-    ro.build.date.utc=0
+BUILD_FINGERPRINT := DOOGEE/S98Pro_EEA/S98Pro:12/SP1A.210812.016/1665735886:user/release-keys
