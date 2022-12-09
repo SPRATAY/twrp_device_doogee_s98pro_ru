a8786a11438ac533a34330a6ee71d40f736fcddf
SPRATAY <34630787+SPRATAY@users.noreply.github.com>
2022-12-09 20:59:44 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,7 @@ DEVICE_PATH := device/doogee/S98Pro
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
+TARGET_SUPPORTS_64_BIT_APPS := true
 
 # Architecture
 TARGET_ARCH := arm64
