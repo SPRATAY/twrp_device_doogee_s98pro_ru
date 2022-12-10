9f784f1aab90a424d5d143601ce8c7aaf2d4cc53
SPRATAY <34630787+SPRATAY@users.noreply.github.com>
2022-12-10 22:55:32 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,7 +107,7 @@ TW_INCLUDE_FBE_METADATA_DECRYPT := true
 
 #test
 #TW_OEM_BUILD := true
-#BOARD_RAMDISK_USE_LZMA := true
+BOARD_RAMDISK_USE_LZMA := true
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
