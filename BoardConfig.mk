6125305e6036f78fb04c415e51597c90e5a0daff
SPRATAY <34630787+SPRATAY@users.noreply.github.com>
2022-12-22 22:34:14 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,8 +118,8 @@ PLATFORM_VERSION := 16.1.0
 #TW_OEM_BUILD := true
 #BOARD_RAMDISK_USE_XZ := true
 TW_EXCLUDE_NANO := true
-#TW_EXCLUDE_BASH := true
-#TW_EXCLUDE_TZDATA := true
+TW_EXCLUDE_BASH := true
+TW_EXCLUDE_TZDATA := true
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
