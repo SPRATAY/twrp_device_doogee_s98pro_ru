42134449a7c4c1153fe7f592906fbadfb9e3b58a
SPRATAY <34630787+SPRATAY@users.noreply.github.com>
2023-01-01 22:32:28 +0300

    Update twrp_S98Pro.mk

--- twrp_S98Pro.mk
+++ twrp_S98Pro.mk
@@ -9,11 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
-
-# work $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
