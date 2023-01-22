86256a9615d9443cd95786c3984ca46f069bd37a
SPRATAY <34630787+SPRATAY@users.noreply.github.com>
2023-01-22 16:26:37 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -75,5 +75,5 @@ PRODUCT_PACKAGES += \
     update_verifier \
     update_engine_sideload
 
-#TW_OVERRIDE_SYSTEM_PROPS := \
+TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
